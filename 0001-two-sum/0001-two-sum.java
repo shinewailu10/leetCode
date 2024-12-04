b2752e07bf601cbfd5d0ce6fcd0faea73c63de44
Shine Wai Lu <120560614+shinewailu10@users.noreply.github.com>
2024-12-04 16:51:57 +0800

    Time: 49 ms (13.54%), Space: 44.9 MB (51.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> result = new ArrayList<>();
+        int[] result = new int[2];
 
         for (int i = 0; i < nums.length; i++){
             for(int j = i+1; j< nums.length; j++){
                 if (nums[i] + nums[j] == target){
-                    result.add(i);
-                    result.add(j);
-                    break;
+                    result[0]=i;
+                    result[1]=j;
                 }
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray(); 
+        return result; 
     }
 }
